2f14741606a82e7f82b5f83811cb097e9c7b8a26
Andrius Farias <72238314+FariaZss@users.noreply.github.com>
2025-09-10 01:23:53 +0000

    Concluindo validações, restrições e entrega final

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -14,8 +14,6 @@ animal = [
   { nome: "Loco", tipo: "jabuti", brinquedo: ["SKATE", "RATO"]}
 ];
   
-/***************************************/
-
   //SETANDO BRINQUEDOS DAS PESSOAS
 function setBrinquedoP1(brinquedo){
   let brinquedosPessoa1 = brinquedo.split(',');
@@ -26,21 +24,43 @@ function setBrinquedoP2(brinquedo){
   let brinquedosPessoa2 = brinquedo.split(',');
   return brinquedosPessoa2;
 }
-/***************************************/
+
 
 class AbrigoAnimais {
+ 
 
   /* MÉTODOS DE VALIDAÇÕES */
-
+  
   validarAnimais(ordemAnimais){
+
+    //VERIFICA SE EXISTE ANIMAIS DUPLICADOS NA LISTA
+    const aDuplicados = arr => new Set(arr).size !== arr.length;
+      if (aDuplicados(ordemAnimais)) {
+        return false;
+      }
+
     //MAPEIA O ARRAY animal, ASSOCIA a NA PROPRIEDADE NOME 
     // CRIANDO UM NOVO ARRAY COM NOME DOS ANIMAIS
     const nomesValidos = animal.map(a => a.nome);
+
+    //REMOVE ANIMAIS REPETIDOS 
+    const nomesUnicos = [...new Set(nomesValidos)];
+
+    //ATUALIZA A VARIAVEL nomesValidos COM OS NOMES UNICOS
+    nomesValidos.length = 0; //LIMPA O ARRAY
+    nomesValidos.push(...nomesUnicos); //ADICIONA OS NOMES UNICOS
+
     //VALIDA SE OS NOMES EM ordemAnimais ESTÃO NO ARRAY DE NOMES DE ANIMAIS
     return ordemAnimais.every(nome => nomesValidos.includes(nome));
   }
 
   validarBrinquedos(brinquedosP1, brinquedosP2){
+
+    const bDuplicados = arr => new Set(arr).size !== arr.length;
+      if (bDuplicados(brinquedosP1) || bDuplicados(brinquedosP2)) {
+    return false;
+    }
+
     //MAPEIA O ARRAY animal, ASSOCIA b NA PROPRIEDADE BRINQUEDO
     //E COM flatMap CRIA UM NOVO ARRAY PARA GUARDAR TODOS OS BRINQUEDOS
     const brinquedosValidos = animal.flatMap(b => b.brinquedo);
@@ -52,20 +72,66 @@ class AbrigoAnimais {
     return todosBrinquedos.every(brinquedo => brinquedosValidos.includes(brinquedo));
   }
 
+  //COMPARA E VALIDA BRINQUEDOS DA PESSOA COM O ANIMAL
   verificaBrinquedosPessoa(brinquedosPessoa, brinquedosAnimal){
-    return brinquedosAnimal.every(b => brinquedosPessoa.includes(b));
+    let brinquedoFav = 0;
+    let ciclos = 0;
+
+    for(let b of brinquedosPessoa){
+      if(b === brinquedosAnimal[brinquedoFav]){
+        brinquedoFav++;
+        if(brinquedoFav === brinquedosAnimal.length){
+          ciclos++;
+          return true;
+        }
+      }
+    }
+    return ciclos > 0;
   }
 
+  //VALIDA A QUANTIDADE DE ADOCOES DE P1 E P2 - SE FOR MAIOR QUE 3, NÃO ADOTA
+  validaQuantidadeAdocoes(p1Adocao, p2Adocao){
+    if(p1Adocao > 3 || p2Adocao > 3){
+      return {
+        erro: 'Pessoa não pode adotar mais de 3 animais', 
+        lista: null
+      };
+    }
+    return null;
+  }
   /*************************************/
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-    //DECLARA VARIAVEIS
+    //VARIAVEIS QUE RECEBEM OS BRINQUEDOS DAS PESSOAS
     const brinquedosP1 = setBrinquedoP1(brinquedosPessoa1);
     const brinquedosP2 = setBrinquedoP2(brinquedosPessoa2);
+
+     //VARIAVEIS PARA CONTROLE DE ADOÇÃO
+    let p1Adocao = 0;
+    let p2Adocao = 0;
+
+    //OBJETO QUE RECEBE RESULTADO (ERRO OU MENSAEGEM DE ADOÇÃO)
     let resultado = { erro: '', lista: [] }
 
+    //VARIAVEL ORDEM RECEBE MAPEAMENTO DE PROPRIEDADE nome SEPARANDO ITENS NO FORMATO ('STRING,STRING')
+    //E USANDO DO TRIM PARA PULAR OS ESPAÇOS DA STRING
     const ordem = ordemAnimais.split(',').map(nome => nome.trim());
-    ordem.sort();
+    ordem.sort(); //ORDENA VARIAVEL ordem EM ORDEM ALFABETICA
+
+    //OBJETO PARA CHECAR ADOÇÃO 
+    const checaAdocao = () => {
+      const valida = this.validaQuantidadeAdocoes(p1Adocao, p2Adocao);
+      
+      if(p1Adocao.length > 3 || p2Adocao.length > 3){
+        return  {
+          erro: 'Pessoa não pode adotar mais de 3 animais',
+          lista: null
+        };
+      }
+
+      if(valida) return valida;
+      return null;
+    }
     
     /*RESTRIÇÃO SE FOR INFORMADO UM ANIMAL QUE NÃO ESTA CONTIDO NA LISTA*/
     if(!this.validarAnimais(ordem)){
@@ -81,45 +147,82 @@ class AbrigoAnimais {
       return resultado;
     }
 
+    //EXECUTA ADOÇAO DE ANIMAIS POR PESSOA 1 E PESSOA 2
     for(let i = 0; i < ordem.length; i++){
       const animalAtual = animal.find(a => a.nome === ordem[i]);
       let brinquedosAnimal = [];
       
+      //SE ANIMAL INFORMADO NÃO ESTIVER NO ARRAY DE ANIMAIS, RETORNA ERRO.
       if(!animalAtual){
          resultado.lista[i] = `${ordem[i]} - animal não encontrado`;
          continue;
       }
 
-      brinquedosAnimal = animalAtual.brinquedo;
-
-      if(this.verificaBrinquedosPessoa(brinquedosP1, brinquedosAnimal)){
-        resultado.lista[i] = `${animalAtual.nome} - pessoa 1`;
-      } else if(this.verificaBrinquedosPessoa(brinquedosP2, brinquedosAnimal)){
-        resultado.lista[i] = `${animalAtual.nome} - pessoa 2`;
-      } else {
-        resultado.lista[i] = `${animalAtual.nome} - abrigo`;
+      //VERIFICA SE O ANIMAL É UM JABUTI E SE HÁ OUTROS ANIMAIS NA LISTA
+      if(animalAtual.tipo === "jabuti" && ordem.length < 2){ 
+        return {
+          erro: 'Jabuti precisa de mais um animal para adoção',
+          lista: null
+        }
       }
-    }
 
+      //VERIFICA SE O ANIMAL É UM GATO PARA NÃO DIVIDIR BRINQUEDOS
+      if(animalAtual.tipo === "gato"){ 
+        const p1Gatos = this.verificaBrinquedosPessoa(brinquedosP1, animalAtual.brinquedo);
+        const p2Gatos = this.verificaBrinquedosPessoa(brinquedosP2, animalAtual.brinquedo);
+        
+          if ((p1Gatos) && (p2Gatos)){
+            resultado.lista[i] = `${animalAtual.nome} - abrigo`;
+          }else if (p1Gatos && !p2Gatos){
+            p1Adocao++;
+            const erro = checaAdocao(p1Adocao, p2Adocao);
+            if(erro) return erro;
+            
+            resultado.lista[i] = `${animalAtual.nome} - pessoa 1`;
 
-    /* CONTINUAÇAO - 
-    
-    NECESSARIO IMPLEMENTAR FUNCIONALIDADE 
-      ALTERNAR BRINQUEDOS
-    
-    NECESSÁRIO IMPLEMENTAR RESTRIÇAO DE GATOS
-      RESTRIÇAO DO LOCO - JABUTI
+          } else if (!p1Gatos && p2Gatos){
+            p2Adocao++;
+            const erro = checaAdocao(p1Adocao, p2Adocao);
+            if(erro) return erro;
 
-    NECESSARIO IMPLEMENTAR VALIDAÇAO
-      QUANTIDADE ADOTADOS POR PESSOA - SE MAIOR QUE 3, NÃO ADOTA
-      SE AMBAS TIVEREM CONDIÇAO - NÃO ADOTA
+            resultado.lista[i] = `${animalAtual.nome} - pessoa 2`;
+
+          } else {
+
+            resultado.lista[i] = `${animalAtual.nome} - abrigo`;
+          
+          }
+        continue;
+      }
     
-    */
+      //ATRIBUI OS BRINQUEDOS DO ANIMAL A VARIAVEL brinquedosAnimal
+      brinquedosAnimal = animalAtual.brinquedo;
+      
+      //FAZ A VALIDAÇAO E VERIFICAÇÃO SE P1 E P2 TEM OS BRINQUEDOS DOS ANIMAIS, SE TEM MAIS DE 3 ADOCOES OU NÃO E DEFINE ADOÇAO OU ABRIGO
+      if(this.verificaBrinquedosPessoa(brinquedosP1, brinquedosAnimal) && this.verificaBrinquedosPessoa(brinquedosP2, brinquedosAnimal)){
+          resultado.lista[i] = `${animalAtual.nome} - abrigo`;
+      } else if(this.verificaBrinquedosPessoa(brinquedosP1, brinquedosAnimal)){
+        
+          p1Adocao++;
+          const erro = checaAdocao(p1Adocao, p2Adocao);
+          if(erro) return erro;
+          
+          resultado.lista[i] = `${animalAtual.nome} - pessoa 1`;
+      } else if(this.verificaBrinquedosPessoa(brinquedosP2, brinquedosAnimal)){
+
+          p2Adocao++;
+          const erro = checaAdocao(p1Adocao, p2Adocao);
+          if(erro) return erro;
+
+          resultado.lista[i] = `${animalAtual.nome} - pessoa 2`;
+      } else {
+          resultado.lista[i] = `${animalAtual.nome} - abrigo`;
+      }
+    } 
 
     return resultado;
-  }
-  /***************************************/
-  
-}  
+    }
+  }  
+/***************************************/
 
 export { AbrigoAnimais as AbrigoAnimais };
--- src/abrigo-animais.test.js
+++ src/abrigo-animais.test.js
@@ -9,8 +9,7 @@ describe('Abrigo de Animais', () => {
     expect(resultado.lista).toBeFalsy();
   });
   
-
-test('Deve encontrar pessoa para um animal', () => {
+  test('Deve encontrar pessoa para um animal', () => {
   const resultado = new AbrigoAnimais().encontraPessoas(
     'RATO,BOLA', 'RATO,NOVELO', 'Rex,Fofo');
     expect(resultado.lista[0]).toBe('Fofo - abrigo');
@@ -36,4 +35,42 @@ test('Deve encontrar pessoa para um animal', () => {
     expect(resultado.erro).toBe('Brinquedo inválido');
     expect(resultado.lista).toBeFalsy();
   });
+
+  test('Deve rejeitar brinquedos duplicados', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('CAIXA,RATO,RATO', 'RATO,BOLA', 'Rex');
+    expect(resultado.erro).toBe('Brinquedo inválido');
+    expect(resultado.lista).toBeFalsy();
+  });
+
+  test('Deve rejeitar animais duplicados', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('CAIXA,RATO', 'RATO,BOLA', 'Rex,Rex,Fofo,Mimi');
+    expect(resultado.erro).toBe('Animal inválido');
+    expect(resultado.lista).toBeFalsy();
+  });
+
+  test('Deve rejeitar a adoção do Loco caso não tenha outros animais', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('LASER,NOVELO', 'SKATE,RATO', 'Loco');
+    expect(resultado.erro).toBe('Jabuti precisa de mais um animal para adoção');
+    expect(resultado.lista).toBeFalsy();
+  });
+
+  test('Deve rejeitar a adoção de gatos caso divida os brinquedos', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('RATO,BOLA', 'BOLA,LASER', 'Zero,Fofo');
+    expect(resultado.lista[0]).toBe('Fofo - abrigo');
+    expect(resultado.lista[1]).toBe('Zero - pessoa 1');
+    expect(resultado.erro).toBeFalsy();
+  });
+
+  test('Rejeita a adoção de novos animais caso P1 ou P2 atinja limite de 3 adoções', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('LASER,RATO,BOLA,CAIXA,NOVELO,SKATE', 'BOLA,LASER,SKATE,NOVELO', 'Rex,Mimi,Bola,Bebe,Loco');
+    expect(resultado.lista[0]).toBe('Bebe - pessoa 1');
+    expect(resultado.lista[1]).toBe('Bola - pessoa 1');
+    expect(resultado.lista[2]).toBe('Loco - abrigo');
+    expect(resultado.lista[3]).toBe('Mimi - pessoa 2');
+    expect(resultado.lista[4]).toBe('Rex - pessoa 1');
+    expect(resultado.erro).toBe('Pessoa não pode adotar mais de 3 animais');
+    expect(resultado.lista).toBeNull();
+  });
+
+
 });
